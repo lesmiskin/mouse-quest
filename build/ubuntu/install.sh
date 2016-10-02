f4f1bfc8830791e5cfb48781db8b40d70b007c9b
Les Miskin <trilinear.nz@gmail.com>
2016-10-02 22:46:04 +1300

    Build stuff

--- build/ubuntu/install.sh
+++ build/ubuntu/install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-    echo "Please run this script with root privileges. e.g. 'sudo ./install-ubuntu.sh'"
+    echo "Please run this script with root privileges. e.g. 'sudo ./install.sh'"
     exit
 fi
 
-apt-get install libsdl2-2.0-0 libsdl2-image-2.0-0 libsdl2-mixer-2.0-0 -y || echo 'Sorry, I could not install the support files. Are you connected to the internet?' &&
+apt-get install libsdl2-2.0-0 libsdl2-image-2.0-0 libsdl2-mixer-2.0-0 -y
 
-echo
-echo 'Supporting files have been installed, thank you for your patience.'
 echo
 echo 'Prepare to enter Mouse Quest! [Press any key]'
 read -n 1
